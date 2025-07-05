422cf3c44c8f6cdbef6ed9742659d492c5066117
Marko489 <89164660+Marko489@users.noreply.github.com>
2025-07-05 19:52:56 +0200

    Delete src/app/api/upload-url directory

--- src/app/api/upload-url/route.ts
+++
@@ -1,9 +0,0 @@
-import { put, generateUploadUrl } from '@vercel/blob';
-
-export async function GET() {
-  const { url } = await generateUploadUrl();
-  return new Response(JSON.stringify({ url }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
